1bc32cfcd6bac2af73a68bfbaa8d6e07c5b55822
adyasha_0405 <154016181+Adyasha-Das-0405@users.noreply.github.com>
2025-08-09 00:21:10 +0530

    Time: 1 ms (84.83%), Space: 9.8 MB (88.83%) - LeetHub

--- 0826-soup-servings/0826-soup-servings.cpp
+++ 0826-soup-servings/0826-soup-servings.cpp
@@ -1,18 +1,26 @@
-double dp[201][201];
 class Solution {
-public: 
-    double dfs(int A, int B){
-        if (A<=0 && B>0) return 1 ;
-        if (A<=0 && B<=0) return 0.5 ;
-        if (A>0 && B<=0) return 0 ;
-        if (dp[A][B]!=-1) return dp[A][B] ;
-        double ans=0.25*(dfs(A-4, B)+dfs(A-3, B-1)+dfs(A-2, B-2)+dfs(A-1, B-3)) ;
-        return dp[A][B]=ans ;
-    }
+    vector<vector<double>> cache;
+public:
     double soupServings(int n) {
-        if (n>5000) return 1 ;
-        fill(&dp[0][0], &dp[0][0]+201*201, -1) ;
-        int N=ceil(n/25.0) ;
-        return dfs(N, N) ;
+        if (n > 5000) return 1.0;
+        int units = (n + 24) / 25;
+        cache.assign(units + 1, vector<double>(units + 1, -1.0));
+        return calcProb(units, units);
+    }
+
+private:
+    double calcProb(int soupA, int soupB) {
+        if (soupA <= 0 && soupB <= 0) return 0.5;
+        if (soupA <= 0) return 1.0;
+        if (soupB <= 0) return 0.0;
+        if (cache[soupA][soupB] != -1.0) return cache[soupA][soupB];
+        double prob = 0.25 * (
+            calcProb(soupA - 4, soupB) +
+            calcProb(soupA - 3, soupB - 1) +
+            calcProb(soupA - 2, soupB - 2) +
+            calcProb(soupA - 1, soupB - 3)
+        );
+        cache[soupA][soupB] = prob;
+        return prob;
     }
 };
