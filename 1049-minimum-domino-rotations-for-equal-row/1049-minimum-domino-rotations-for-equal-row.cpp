5d9bd740e4cfd28ae2df12b112d6b4b10b7942d9
adyasha_0405 <154016181+Adyasha-Das-0405@users.noreply.github.com>
2025-09-29 14:02:15 +0530

    Time: 0 ms (100%), Space: 115.7 MB (79.19%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
-    static int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        const int n=tops.size(), m=bottoms.size();
-        if (n!=m) return -1;// edge case
-        int T[7]={0}, B[7]={0}, S[7]={0};
-        for(int i=0; i<n; i++){
-            T[tops[i]]++;
-            B[bottoms[i]]++;
-            S[tops[i]]+=(tops[i]==bottoms[i]);
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        const int n = tops.size();
+
+        // Try to make all values equal to tops[0] or bottoms[0]
+        int X=tops[0], Y=bottoms[0];
+        int swapXT=0, swapXB=0;
+        int swapYT=0, swapYB=0;
+
+        bool hasX=1, hasY=1;
+
+        for (int i=0; i<n; i++) {
+            // Check for candidate X
+            if (hasX) {
+                if (tops[i]!=X && bottoms[i]!=X) 
+                    hasX=0;
+                else {
+                    if (tops[i]!=X) swapXT++;
+                    if (bottoms[i]!=X) swapXB++;
+                }
+            }
+
+            // Check for candidate Y
+            if (hasY) {
+                if (tops[i]!=Y && bottoms[i]!=Y) 
+                    hasY=0;
+                else {
+                    if (tops[i]!=Y) swapYT++;
+                    if (bottoms[i]!=Y) swapYB++;
+                }
+            }
+
+            // Early exit if both invalid
+            if (!hasX && !hasY) return -1;
         }
+
         int ans=INT_MAX;
-        for(int x=1; x<=6; x++){
-            if (T[x]+B[x]-S[x]==n)
-                ans=min(ans, min(n-T[x], n-B[x]));
-        }
-        return ans==INT_MAX?-1:ans;
+        if (hasX) ans=min(ans, min(swapXT, swapXB));
+        if (hasY) ans=min(ans, min(swapYT, swapYB));
+
+        return ans;
     }
 };
