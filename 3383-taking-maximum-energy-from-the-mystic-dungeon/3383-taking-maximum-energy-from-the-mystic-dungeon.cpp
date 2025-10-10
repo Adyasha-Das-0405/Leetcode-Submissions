b5e37f949cf31192a4da41706c8abd398f6b51b8
adyasha_0405 <154016181+Adyasha-Das-0405@users.noreply.github.com>
2025-10-10 09:31:05 +0530

    Time: 151 ms (25%), Space: 145.4 MB (77.46%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
         int n = energy.size();
-    vector<int> dp(n, 0);
     int maxEnergy = INT_MIN;
 
-    // Fill dp from right to left
+    // Traverse from right to left
     for (int i = n - 1; i >= 0; i--) {
-        // Base energy from current magician
-        dp[i] = energy[i];
+        // If jump is valid, add the energy of the magician k steps ahead
+        if (i + k < n) {
+            energy[i] += energy[i + k];
+        }
 
-        // If we can jump k steps ahead, add that energy
-        if (i + k < n) dp[i] += dp[i + k];
-
-        // Track the maximum so far
-        maxEnergy = max(maxEnergy, dp[i]);
+        // Keep track of the maximum energy possible
+        maxEnergy = max(maxEnergy, energy[i]);
     }
 
     return maxEnergy;
