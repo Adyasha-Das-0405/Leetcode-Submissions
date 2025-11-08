7d0db756934c5c6d2c219f70bd0a3d214a867d1a
adyasha_0405 <154016181+Adyasha-Das-0405@users.noreply.github.com>
2025-11-08 13:55:44 +0530

    Time: 23 ms (94.42%), Space: 11.4 MB (99.49%) - LeetHub

--- 3761-maximum-difference-between-even-and-odd-frequency-ii/3761-maximum-difference-between-even-and-odd-frequency-ii.cpp
+++ 3761-maximum-difference-between-even-and-odd-frequency-ii/3761-maximum-difference-between-even-and-odd-frequency-ii.cpp
@@ -1,59 +1,40 @@
-class Solution 
-{
+class Solution {
 public:
-    int maxDifference(string s, int k) 
-    {
+    int maxDifference(string s, int k) {
+        auto getStatus = [](int cnt_a, int cnt_b) -> int {
+            return ((cnt_a & 1) << 1) | (cnt_b & 1);
+        };
+
         int n = s.size();
         int ans = INT_MIN;
-
-        // Step 1: Try all (a, b) pairs
-        for (int a = 0; a <= 4; ++a) 
-        {
-            for (int b = 0; b <= 4; ++b) 
-            {
-                if (a == b)
-                {
+        for (char a = '0'; a <= '4'; ++a) {
+            for (char b = '0'; b <= '4'; ++b) {
+                if (a == b) {
                     continue;
-                } 
-
-                vector<int> s1(n + 1);
-                vector<int> s2(n + 1);
-                
-                // Step 2: Prefix counts
-                for (int i = 1; i <= n; ++i) 
-                {
-                    s1[i] = s1[i - 1] + (s[i - 1] - '0' == a);
-                    s2[i] = s2[i - 1] + (s[i - 1] - '0' == b);
                 }
-
-                // Step 3: Matrix for best difference at each parity
-                int g[2][2] = {{INT_MIN, INT_MIN}, {INT_MIN, INT_MIN}};
-                int j = 0;
-
-                // Step 4: Sliding window
-                for (int i = k; i <= n; ++i) 
-                {
-                    while (i - j >= k && s1[i] > s1[j] && s2[i] > s2[j]) 
-                    {
-                        int pa = s1[j] % 2;
-                        int pb = s2[j] % 2;
-                        g[pa][pb] = max(g[pa][pb], s2[j] - s1[j]);
-                        ++j;
+                int best[4] = {INT_MAX, INT_MAX, INT_MAX, INT_MAX};
+                int cnt_a = 0, cnt_b = 0;
+                int prev_a = 0, prev_b = 0;
+                int left = -1;
+                for (int right = 0; right < n; ++right) {
+                    cnt_a += (s[right] == a);
+                    cnt_b += (s[right] == b);
+                    while (right - left >= k && cnt_b - prev_b >= 2) {
+                        int left_status = getStatus(prev_a, prev_b);
+                        best[left_status] =
+                            min(best[left_status], prev_a - prev_b);
+                        ++left;
+                        prev_a += (s[left] == a);
+                        prev_b += (s[left] == b);
                     }
-
-                    // Step 5: Check if valid
-                    int pa = s1[i] % 2;
-                    int pb = s2[i] % 2;
-                    int need = g[1 - pa][pb];
-                    
-                    if (need != INT_MIN) 
-                    {
-                        ans = max(ans, (s1[i] - s2[i]) + need);
+                    int right_status = getStatus(cnt_a, cnt_b);
+                    if (best[right_status ^ 0b10] != INT_MAX) {
+                        ans =
+                            max(ans, cnt_a - cnt_b - best[right_status ^ 0b10]);
                     }
                 }
             }
         }
-
-        return ans == INT_MIN ? -1 : ans;
+        return ans;
     }
 };
