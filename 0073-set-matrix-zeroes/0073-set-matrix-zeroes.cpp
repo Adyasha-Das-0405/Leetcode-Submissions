9bcc15780751d45b4ed073c84287b3b29e66d9b8
adyasha_0405 <154016181+Adyasha-Das-0405@users.noreply.github.com>
2026-06-08 22:39:09 +0530

    Time: 0 ms (100%), Space: 20.6 MB (99.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool zeroinFirstCol = false;
-        for (int row = 0; row < matrix.size(); row++) {
-            if (matrix[row][0] == 0) zeroinFirstCol = true;
-            for (int col = 1; col < matrix[0].size(); col++) {
-                if (matrix[row][col] == 0) {
-                    matrix[row][0] = 0;
-                    matrix[0][col] = 0;
+        // Your code goes here
+        int m = matrix.size(), n = matrix[0].size() ;
+
+        int colvar = 1 ;
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++) {
+                if(matrix[i][j] == 0) {
+                    //mark i-th row
+                    matrix[i][0] = 0 ;
+                    //mark j=th col
+                    if(j != 0){
+                        matrix[0][j] = 0 ;
+                    } else {
+                        colvar = 0 ;
+                    }
                 }
             }
         }
 
-        for (int row = matrix.size() - 1; row >= 0; row--) {
-            for (int col = matrix[0].size() - 1; col >= 1; col--) {
-                if (matrix[row][0] == 0 || matrix[0][col] == 0) {
-                    matrix[row][col] = 0;
+        for(int i = 1 ; i < m ; i++) {
+            for(int j = 1 ; j < n ; j++) {
+                if(matrix[i][j] != 0) {
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0 ;
+                    }
                 }
             }
-            if (zeroinFirstCol) {
-                matrix[row][0] = 0;
+        }
+        if(matrix[0][0] == 0){
+            for(int j = 0 ; j < n ; j++) matrix[0][j] = 0 ;
+        }
+        if(colvar == 0) {
+            for(int i = 0 ; i < m ; i++) {
+                matrix[i][0] = 0 ;
             }
         }
     }
